8075a77453910ff5d895e4d48061c3ec47d1edde
= <ccreyesmolina@gmail.com>
2024-04-18 23:00:00 -0500

    Add authentication guard to login route

--- src/app/public/auth/auth-routing.module.ts
+++ src/app/public/auth/auth-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutAuthComponent } from '../../ui/layouts/layout-auth/layout-auth.component';
 import { LoginComponent } from '../../ui/forms/login/login.component';
+import { authGuard } from './auth.guard';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LayoutAuthComponent,
     children: [{ path: '', component: LoginComponent, outlet: 'auth-form' }],
+    canActivate: [authGuard],
   },
 ];
 
