b43a69b4c4c678c0ee87f0eeea54c6c0c8a8eabd
= <ccreyesmolina@gmail.com>
2024-04-18 19:10:37 -0500

    Add routing for 'quotes' path in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'quotes',
+    loadChildren: () =>
+      import('./private/private.module').then((module) => module.PrivateModule),
+  },
+  {
     path: '',
     loadChildren: () =>
       import('./public/public.module').then((module) => module.PublicModule),
