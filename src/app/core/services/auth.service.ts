3bac579d82d472f4192df87849d7f6e652b09d6f
= <ccreyesmolina@gmail.com>
2024-04-19 15:08:52 -0500

    Update AuthService to include storage of token in login method

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from './generals/http.service';
 import { ApiToAuthResponseMapper } from '../mappers/api-to-auth-response.mapper';
-import { Observable, map } from 'rxjs';
+import { Observable, map, pipe, tap } from 'rxjs';
 import { URL_RESOURCES } from '../resources/url.resources';
 import { IAuthResponseModel } from '../models/auth-response.model';
+import { StorageService } from './generals/storage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,16 +12,19 @@ import { IAuthResponseModel } from '../models/auth-response.model';
 export class AuthService {
   constructor(
     private readonly httpService: HttpService,
+    private readonly storageService: StorageService,
     private readonly mapper: ApiToAuthResponseMapper
   ) {}
 
   login(formData): Observable<IAuthResponseModel> {
     const url = URL_RESOURCES.login;
-    return this.httpService.post<IAuthResponseModel>(url, formData).pipe(
-      map((response) => {
-        console.log(response);
-        return this.mapper.map(response);
-      })
-    );
+    const result = this.httpService
+      .post<IAuthResponseModel>(url, formData)
+      .pipe(
+        map((response) => this.mapper.map(response)),
+        tap((response) => this.storageService.set('TOKEN', response.token))
+      );
+
+    return result;
   }
 }
