e157bdcc233f0f3505de47f7bdaa8c210b4da5a3
= <ccreyesmolina@gmail.com>
2024-04-18 19:10:21 -0500

    Add authentication guard and routing modules to redirect if user already authenticated

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./auth/auth-routing.module').then(
+        (module) => module.AuthRoutingModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PublicRoutingModule { }
+export class PublicRoutingModule {}
