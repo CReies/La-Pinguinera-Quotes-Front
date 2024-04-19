f6e8d6bd7ef28f1591b675a56324efd0fb0369aa
= <ccreyesmolina@gmail.com>
2024-04-19 05:37:22 -0500

    Update login component to use reactive forms

--- src/app/ui/forms/login/login.component.ts
+++ src/app/ui/forms/login/login.component.ts
@@ -1,21 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { TitleComponent } from '../../elements/title/title.component';
-import { InputComponent } from '../../elements/input/input.component';
 import { ButtonComponent } from '../../elements/button/button.component';
 import { LabelComponent } from '../../elements/label/label.component';
 import { AnchorComponent } from '../../elements/anchor/anchor.component';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     TitleComponent,
-    InputComponent,
     ButtonComponent,
     LabelComponent,
     AnchorComponent,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  @Output() login = new EventEmitter();
+
+  form: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  sendInfo($event: SubmitEvent) {
+    $event.preventDefault();
+    this.login.emit(this.form.getRawValue());
+  }
+}
