07845e7c21f6789397558be3e33d7c5979b30915
= <ccreyesmolina@gmail.com>
2024-04-20 06:46:31 -0500

    Refactor list-all-books-container component to use books$ observable and add ListAllBooksContainerFacade

--- src/app/containers/list-all-books-container/list-all-books-container.component.ts
+++ src/app/containers/list-all-books-container/list-all-books-container.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+// import { ListAllBooksComponent } from '../../ui/blocks/list-all-books/list-all-books.component';
+
+// @Component({
+//   selector: 'app-list-all-books-container',
+//   standalone: true,
+//   imports: [ListAllBooksComponent],
+//   templateUrl: './list-all-books-container.component.html',
+// })
+// export class ListAllBooksContainerComponent {
+
+// }
+
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IBook } from '../../core/models/book.model';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 import { ListAllBooksComponent } from '../../ui/blocks/list-all-books/list-all-books.component';
+import { ListAllBooksContainerFacade } from './list-all-books-container.facade';
 
 @Component({
-  selector: 'app-list-all-books-container',
+  selector: 'app-budget-list-all-container',
   standalone: true,
-  imports: [ListAllBooksComponent],
+  imports: [ListAllBooksComponent, AsyncPipe],
   templateUrl: './list-all-books-container.component.html',
 })
-export class ListAllBooksContainerComponent {
+export class ListAllBooksContainerComponent implements OnInit, OnDestroy {
+  public books$: Observable<IBook[]>;
 
+  constructor(private readonly facade: ListAllBooksContainerFacade) {}
+
+  ngOnInit(): void {
+    this.facade.initSubscriptions();
+    this.facade.getBooks();
+    this.initializeSubscriptions();
+  }
+  ngOnDestroy(): void {
+    this.facade.destroySubscriptions();
+  }
+
+  private initializeSubscriptions(): void {
+    this.books$ = this.facade.books$();
+  }
 }
---
+++ src/app/containers/list-all-books-container/list-all-books-container.facade.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subscription, tap } from 'rxjs';
+import { AppState } from '../../core/state/app.state';
+import { BooksService } from '../../core/services/books.service';
+import { IBook } from '../../core/models/book.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ListAllBooksContainerFacade {
+  private subscriptions: Subscription;
+
+  constructor(
+    private readonly appState: AppState,
+    private readonly booksService: BooksService
+  ) {}
+
+  books$(): Observable<IBook[]> {
+    return this.appState.books.books.$();
+  }
+
+  initSubscriptions(): void {
+    this.subscriptions = new Subscription();
+  }
+
+  destroySubscriptions(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  getBooks(): void {
+    this.subscriptions.add(
+      this.booksService
+        .getAll()
+        .pipe(tap(this.appState.books.books.set.bind(this)))
+        .subscribe()
+    );
+  }
+}
