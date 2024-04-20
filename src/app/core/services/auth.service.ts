dd1ee4ab11d53d270039dd178e93d899b9f638d1
= <ccreyesmolina@gmail.com>
2024-04-20 18:32:03 -0500

    Remove unused imports and dependencies

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -5,7 +5,6 @@ import { Observable, map, pipe, tap } from 'rxjs';
 import { URL_RESOURCES } from '../resources/url.resources';
 import { IAuthResponseModel } from '../models/auth-response.model';
 import { StorageService } from './generals/storage.service';
-import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -14,8 +13,7 @@ export class AuthService {
   constructor(
     private readonly httpService: HttpService,
     private readonly storageService: StorageService,
-    private readonly mapper: ApiToAuthResponseMapper,
-    private readonly route: Router
+    private readonly mapper: ApiToAuthResponseMapper
   ) {}
 
   login(formData): Observable<IAuthResponseModel> {
