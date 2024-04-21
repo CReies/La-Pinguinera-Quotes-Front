2264c24fc63c71e8e37e1e547d59563773feb04b
= <ccreyesmolina@gmail.com>
2024-04-20 20:46:29 -0500

    Refactor add-book component to use reactive forms and emit addBook event

--- src/app/ui/forms/add-book/add-book.component.ts
+++ src/app/ui/forms/add-book/add-book.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { LabelComponent } from '../../elements/label/label.component';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ButtonComponent } from '../../elements/button/button.component';
+import { IBook } from '../../../core/models/book.model';
 
 @Component({
   selector: 'app-add-book',
   standalone: true,
-  imports: [LabelComponent],
+  imports: [LabelComponent, ButtonComponent, ReactiveFormsModule],
   templateUrl: './add-book.component.html',
-  styleUrl: './add-book.component.css'
+  styleUrl: './add-book.component.css',
 })
-export class AddBookComponent {
+export class AddBookComponent implements OnInit {
+  @Output() addBook = new EventEmitter();
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  form: FormGroup = new FormGroup({
+    title: new FormControl(''),
+    author: new FormControl(''),
+    price: new FormControl(0),
+    type: new FormControl(''),
+  });
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      title: ['', [Validators.required]],
+      author: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      type: ['', [Validators.required]],
+    });
+  }
+
+  sendInfo($event: SubmitEvent) {
+    $event.preventDefault();
+    if (this.form.invalid) {
+      alert('Please fill in the form correctly');
+      return;
+    }
+
+    this.addBook.emit(this.form.getRawValue());
+  }
 }
