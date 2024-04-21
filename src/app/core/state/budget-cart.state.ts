c0f81f41106174b5d1ffe7ab05b9e31812beb1e2
= <ccreyesmolina@gmail.com>
2024-04-21 16:28:09 -0500

    Add IBudgetQuoteRequest interface for budget quote requests

--- src/app/core/state/budget-cart.state.ts
+++ src/app/core/state/budget-cart.state.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { StateFactory } from './factory.state';
 import { BehaviorSubject } from 'rxjs';
+import { IBudgetQuoteRequest } from '../models/budget-quote-request.model';
+import { IBook } from '../models/book.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BudgetCartState {
-  private books$: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
+  private books$: BehaviorSubject<IBook[]> = new BehaviorSubject<IBook[]>([]);
 
   constructor(private readonly factory: StateFactory) {}
 
