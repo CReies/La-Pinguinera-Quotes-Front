5c85fb38cccafea8fd9c60cc6053f5bd964865a5
= <ccreyesmolina@gmail.com>
2024-04-21 16:27:57 -0500

    Refactor budget-added-books component to render a list of books

--- src/app/ui/blocks/budget-added-books/budget-added-books.component.ts
+++ src/app/ui/blocks/budget-added-books/budget-added-books.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IBook } from '../../../core/models/book.model';
+import { TitleComponent } from '../../elements/title/title.component';
+import { BookListElementComponent } from '../book-list-element/book-list-element.component';
 
 @Component({
   selector: 'app-budget-added-books',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, BookListElementComponent],
   templateUrl: './budget-added-books.component.html',
-  styleUrl: './budget-added-books.component.css'
+  styleUrl: './budget-added-books.component.css',
 })
 export class BudgetAddedBooksComponent {
-
+  @Input() books: IBook[];
 }
