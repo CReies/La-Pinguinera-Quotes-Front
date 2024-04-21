e41ca25b844da068e65af238d7ab0dedfe07c9e8
= <ccreyesmolina@gmail.com>
2024-04-21 13:48:14 -0500

    Update list-quoted-books.component.html and list-quoted-books.component.ts to include quoteData in app-list-quoted-books component

--- src/app/ui/blocks/list-quoted-books/list-quoted-books.component.ts
+++ src/app/ui/blocks/list-quoted-books/list-quoted-books.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IBook } from '../../../core/models/book.model';
 import { TitleComponent } from '../../elements/title/title.component';
 import { BookListElementComponent } from '../book-list-element/book-list-element.component';
+import { IListQuoteResponse } from '../../../core/models/list-quote-response.model';
 
 @Component({
   selector: 'app-list-quoted-books',
@@ -11,36 +12,5 @@ import { BookListElementComponent } from '../book-list-element/book-list-element
   styleUrl: './list-quoted-books.component.css',
 })
 export class ListQuotedBooksComponent {
-  books: IBook[] = [
-    {
-      title: 'Book 1',
-      author: 'Author 1',
-      price: 10,
-      type: 'Novel',
-    },
-    {
-      title: 'Book 1',
-      author: 'Author 1',
-      price: 10,
-      type: 'Novel',
-    },
-    {
-      title: 'Book 1',
-      author: 'Author 1',
-      price: 10,
-      type: 'Novel',
-    },
-    {
-      title: 'Book 1',
-      author: 'Author 1',
-      price: 10,
-      type: 'Novel',
-    },
-    {
-      title: 'Book 1',
-      author: 'Author 1',
-      price: 10,
-      type: 'Novel',
-    },
-  ];
+  @Input() quoteData: IListQuoteResponse;
 }
