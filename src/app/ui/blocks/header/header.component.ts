36fe95c93b56844db2f3710a29172d4ec2b32b0f
= <ccreyesmolina@gmail.com>
2024-04-23 04:37:17 -0500

    Update header component styles and HTML structure

--- src/app/ui/blocks/header/header.component.ts
+++ src/app/ui/blocks/header/header.component.ts
@@ -2,11 +2,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TitleComponent } from '../../elements/title/title.component';
 import { IAuthResponseModel } from '../../../core/models/auth-response.model';
 import { SvgComponent } from '../../elements/svg/svg.component';
+import { AnchorComponent } from '../../elements/anchor/anchor.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TitleComponent, SvgComponent],
+  imports: [SvgComponent, AnchorComponent, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
