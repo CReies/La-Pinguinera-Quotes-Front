390227baf13f4da1896c7c6d9289c11fc1543d04
= <ccreyesmolina@gmail.com>
2024-04-24 11:36:50 -0500

    Add form validation error messages in add-book component

--- src/app/ui/forms/add-book/add-book.component.ts
+++ src/app/ui/forms/add-book/add-book.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { LabelComponent } from '../../elements/label/label.component';
 import {
+  AbstractControl,
   FormBuilder,
   FormControl,
   FormGroup,
@@ -19,6 +20,8 @@ import { ButtonComponent } from '../../elements/button/button.component';
 export class AddBookComponent implements OnInit {
   @Output() addBook = new EventEmitter();
 
+  submitted: boolean = false;
+
   constructor(private formBuilder: FormBuilder) {}
 
   form: FormGroup = new FormGroup({
@@ -30,20 +33,32 @@ export class AddBookComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      title: ['', [Validators.required]],
-      author: ['', [Validators.required]],
-      price: ['', [Validators.required]],
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      author: ['', [Validators.required, Validators.minLength(3)]],
+      price: ['', [Validators.required, Validators.min(0)]],
       type: ['', [Validators.required]],
     });
   }
 
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  resetForm() {
+    this.form.reset();
+    this.submitted = false;
+  }
+
   sendInfo($event: SubmitEvent) {
+    this.submitted = true;
     $event.preventDefault();
+    console.log(this.form);
+
     if (this.form.invalid) {
-      alert('Please fill in the form correctly');
       return;
     }
 
     this.addBook.emit(this.form.getRawValue());
+    this.resetForm();
   }
 }
