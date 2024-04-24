08475c8ad5e4bf22190ed8618e53e65c84154af9
= <ccreyesmolina@gmail.com>
2024-04-24 12:04:46 -0500

    Add form validation error messages in register component

--- src/app/ui/forms/register/register.component.ts
+++ src/app/ui/forms/register/register.component.ts
@@ -4,6 +4,7 @@ import { ButtonComponent } from '../../elements/button/button.component';
 import { LabelComponent } from '../../elements/label/label.component';
 import { AnchorComponent } from '../../elements/anchor/anchor.component';
 import {
+  AbstractControl,
   FormBuilder,
   FormControl,
   FormGroup,
@@ -27,6 +28,8 @@ import {
 export class RegisterComponent implements OnInit {
   @Output() register = new EventEmitter();
 
+  submitted: boolean = false;
+
   form: FormGroup = new FormGroup({
     email: new FormControl(''),
     username: new FormControl(''),
@@ -37,19 +40,44 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      email: ['', [Validators.required]],
-      username: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      email: ['', [Validators.required, Validators.email]],
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(100),
+        ],
+      ],
     });
   }
 
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  resetForm() {
+    this.form.reset();
+    this.submitted = false;
+  }
+
   sendInfo($event: SubmitEvent) {
+    this.submitted = true;
     $event.preventDefault();
+
     if (this.form.invalid) {
-      alert('Please fill in the form correctly');
       return;
     }
 
     this.register.emit(this.form.getRawValue());
+    this.resetForm();
   }
 }
